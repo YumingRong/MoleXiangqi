342463bffb64db3df028737d7dc8850901d59ba2
Rong Yuming <rym_shunv@msn.com>
2020-02-02 22:04:06 +0800

    Some change. Not debugged.

--- Search.cs
+++ Search.cs
@@ -6,11 +6,18 @@ namespace MoleXiangqi
 {
     partial class POSITION
     {
-        const int MATE_VALUE = 5000;
+        //const int MATE_VALUE = 5000;
         public int SearchQuiesce(int alpha, int beta)
         {
-            int best = -MATE_VALUE;
-            int vl = best;
+            int best;
+            int vl;
+            // 7. 对于未被将军的局面，在生成着法前首先尝试空着(空着启发)，即对局面作评价；
+            vl = Complex_Evaluate();
+            if (vl > beta)
+                return vl;
+            best = vl;
+            alpha = Math.Max(alpha, vl);
+
             List<MOVE> mvs = GenerateCaptures();
             if (mvs.Count == 0)
                 return Complex_Evaluate();
